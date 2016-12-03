2979efedb1dd7031af91cc7fa285f7c7f1b8e684
pwnzorak <pwnzor.ak@gmail.com>
2016-12-03 16:46:21 -0500

    Added header comment to main

--- hackathon-kms/src/hackathon/kms/menu.java
+++ hackathon-kms/src/hackathon/kms/menu.java
@@ -1,8 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/*Nathan Chan
+  Andrew Karapostolakis
+  Steven Nim
+  Seegal Panchal
+  2016-12-03
+  Multi-purpose calculator*/
 package hackathon.kms;
 
 /**
